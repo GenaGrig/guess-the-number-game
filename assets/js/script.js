b166713f13c435c59bad04bf2e2bb9da1ff170be
GenaGrig <gennadiuss@gmail.com>
2023-02-24 20:20:40 +0000

    Add difficulty Hardest, Deleted unused buttons. Deleted JS code from HTML and put it in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,20 @@
 let randomNumberEasy = Math.floor(Math.random() * 10) + 1;
 let randomNumberMedium = Math.floor(Math.random() * 100) + 1;
 let randomNumberHard = Math.floor(Math.random() * 500) + 1;
+let randomNumberVeryHard = Math.floor(Math.random() * 1000) + 1;
 
 let totalGuesses = document.getElementById('total-guesses');
 let lastGuess = document.getElementById('last-guess');
 let lessOrMore = document.getElementById('less-or-more');
 let playerInput = document.getElementById('player-input');
 let playerCheck = document.getElementById('submit-button')
-let currentDifficulty = document.getElementById('show-difficulty');
 let easyCheck = document.getElementById('easy-check');
 let mediumCheck = document.getElementById('medium-check');
 let hardCheck = document.getElementById('hard-check');
+let veryHardCheck = document.getElementById('very-hard-check');
 
 let guessCount = 1;
-let resetButton = document.getElementById('new-game');
+let newGameButton = document.getElementById('new-game');
 
 function checkNumberEasy () {
     let playerGuess = Number(playerInput.value);
@@ -122,6 +123,41 @@ function checkNumberHard () {
 }
 hardCheck.addEventListener('click', checkNumberHard);
 
+function checkNumberHardest () {
+    let playerGuess = Number(playerInput.value);
+
+    if (guessCount === 1) {
+        totalGuesses.textContent = "Your previous guesses are: ";
+        totalGuesses.style.backgroundColor = 'violet';
+    }
+    totalGuesses.textContent += playerGuess + ' ';
+
+    if (playerGuess === randomNumberVeryHard) {
+        lastGuess.textContent = "Congratulations! You guessed the right number!";
+        lastGuess.style.backgroundColor = 'green';
+        lessOrMore.textContent = '';
+        setGameOver();
+    } else if (guessCount === 15) {
+        lastGuess.textContent = `Your attempts are over! Try again! The correct number is ${randomNumberVeryHard}`;
+        setGameOver();
+    } else {
+        lastGuess.textContent = `Nope! Next attempt! You have ${15 - guessCount} guesses left!`;
+        lastGuess.style.backgroundColor = 'red';
+        if (playerGuess < randomNumberVeryHard) {
+            lessOrMore.textContent = 'Your number was lower than guessed!';
+            lessOrMore.style.backgroundColor = 'blue';
+        } else if (playerGuess > randomNumberVeryHard) {
+            lessOrMore.textContent = 'Your number was higher than guessed!';
+            lessOrMore.style.backgroundColor = 'red';
+        }
+    }
+
+    guessCount++;
+    playerInput.value = '';
+    playerInput.focus();
+}
+veryHardCheck.addEventListener('click', checkNumberHardest);
+
 
 
 function setGameOver() {
@@ -134,3 +170,5 @@ function setGameOver() {
   function refreshPage(){
     window.location.reload();
 } 
+
+newGameButton.addEventListener('click', refreshPage)
